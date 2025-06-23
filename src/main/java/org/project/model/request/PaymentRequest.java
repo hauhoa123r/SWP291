c3840bbe9758baef5de68c575524e2d36ba35d5e
nguyen van dong <dongnvhe180365@fpt.edu.vn>
2025-06-23 11:51:53 +0700

    update quan li thanh

---
+++ src/main/java/org/project/model/request/PaymentRequest.java
@@ -0,0 +1,44 @@
+package org.project.model.request;
+
+import java.math.BigDecimal;
+
+public class PaymentRequest {
+
+    private Long prescriptionId;
+    private Long userId;
+    private BigDecimal amount;
+    private String paymentMethod;
+
+
+    public Long getPrescriptionId() {
+        return prescriptionId;
+    }
+
+    public void setPrescriptionId(Long prescriptionId) {
+        this.prescriptionId = prescriptionId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
---
+++ src/main/java/org/project/model/response/PaymentResponse.java
@@ -0,0 +1,62 @@
+package org.project.model.response;
+
+import java.math.BigDecimal;
+
+public class PaymentResponse {
+
+    private Long paymentId;
+    private BigDecimal amount;
+    private String paymentMethod;
+    private String paymentStatus;
+    private String paymentTime;
+    private String orderCode; // Nếu cần, hoặc bỏ qua
+
+    // Getter và Setter
+    public Long getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getPaymentTime() {
+        return paymentTime;
+    }
+
+    public void setPaymentTime(String paymentTime) {
+        this.paymentTime = paymentTime;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+}
